ea4bf3073c53b076e061a05dbab9900c58772b4b
Samshah001 <smrshah007@gmail.com>
2022-10-12 13:04:39 +0530

    Second commit

---
+++ SameerWalks/SameerWalks.API/Data/SameerWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SameerWalks.API.Models.Domain;
+
+namespace SameerWalks.API.Data
+{
+    public class SameerWalksDbContext : DbContext
+    {
+        public SameerWalksDbContext(DbContextOptions<SameerWalksDbContext> options): base(options)
+        {
+
+
+        }
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
